c057196aed153098e3f8a844abe13cae7cb6d586
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-03-24 00:57:51 +0200

    Add files via upload

---
+++ BasicSyntaxConditionalStatementsAndLoops/07.TheatrePromotions/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace _7._Theatre_Promotions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var typeOfDay = Console.ReadLine();
+            var age = int.Parse(Console.ReadLine());
+
+            double priceOfTicket = 0;
+          
+            if (age >= 0 && age <= 18)
+            {
+                if (typeOfDay == "Weekday")
+                {
+                    priceOfTicket = 12;
+                }
+                else if (typeOfDay == "Weekend")
+                {
+                    priceOfTicket = 15;
+                }
+                else if (typeOfDay == "Holiday")
+                {
+                    priceOfTicket = 5;
+                }
+                Console.WriteLine($"{priceOfTicket}$");
+            }
+            else if (age >= 18 && age <= 64)
+            {
+                if (typeOfDay == "Weekday")
+                {
+                    priceOfTicket = 18;
+                }
+                else if (typeOfDay == "Weekend")
+                {
+                    priceOfTicket = 20;
+                }
+                else if (typeOfDay == "Holiday")
+                {
+                    priceOfTicket = 12;
+                }
+                Console.WriteLine($"{priceOfTicket}$");
+            }
+            else if (age >= 64 && age <= 122)
+            {
+                if (typeOfDay == "Weekday")
+                {
+                    priceOfTicket = 12;
+                }
+                else if (typeOfDay == "Weekend")
+                {
+                    priceOfTicket = 15;
+                }
+                else if (typeOfDay == "Holiday")
+                {
+                    priceOfTicket = 10;
+                }
+                Console.WriteLine($"{priceOfTicket}$");
+            }
+            else
+            {
+                Console.WriteLine("Error!");
+            }
+            
+        }
+    }
+}
