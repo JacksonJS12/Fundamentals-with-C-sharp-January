94710cc5f41e8d4a57416cbae500df14a07d2612
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-03-24 00:28:56 +0200

    Add files via upload

---
+++ BasicSyntaxConditionalStatementsAndLoops/01.Student-Information/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _1._Student_Information
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = Console.ReadLine();
+            var age = int.Parse(Console.ReadLine());
+            var grade = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Name: {name}, Age: {age}, Grade: {grade:F2}");
+           
+            
+        }
+    }
+}
