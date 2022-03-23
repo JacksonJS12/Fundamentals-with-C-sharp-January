15f79741ef95a4cd97762205134b4b247451869f
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-03-24 01:07:15 +0200

    Add files via upload

---
+++ BasicSyntaxConditionalStatementsAndLoops/12.EvenNumber/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _12._Even_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var num = int.Parse(Console.ReadLine());
+
+            if (num % 2 == 0)
+            {
+                Console.WriteLine($"The number is: {Math.Abs(num)}");
+                
+            } 
+
+            if (num % 2 != 0)
+            {
+                for (int i = 0; ; i++)
+                {
+                    if (num % 2 == 0)
+                    {
+                        Console.WriteLine($"The number is: {Math.Abs(num)}");
+                        break;
+                    }
+                    Console.WriteLine("Please write an even number.");
+                    num = int.Parse(Console.ReadLine());
+                }
+            }
+            
+
+        }
+    }
+}
