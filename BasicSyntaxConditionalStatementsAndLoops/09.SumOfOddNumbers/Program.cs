db2bd870f5ed8471168d18a17813e3ef286e1e95
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-03-24 01:01:22 +0200

    Add files via upload

---
+++ BasicSyntaxConditionalStatementsAndLoops/09.SumOfOddNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _9._Sum_of_Odd_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var num = int.Parse(Console.ReadLine());
+            var sum = 0;
+
+            for (int i = 0; i < num; i++)
+            {
+                var currentOddNum = 1 + (i * 2);
+                sum += currentOddNum;
+
+                Console.WriteLine(currentOddNum);
+            }
+
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
