6fd29a6fa957220a2d86cb60c7e7baeac97e751c
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-03-24 00:52:13 +0200

    Add files via upload

---
+++ BasicSyntaxConditionalStatementsAndLoops/04.BackIn30Minutes/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _4._Back_in_30_Minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hours = int.Parse(Console.ReadLine());
+            var minutes = int.Parse(Console.ReadLine()) + 30;
+
+            if (minutes > 59)
+            {
+                hours += 1;
+                minutes -= 60;
+            }
+            if (hours > 23)
+            {
+                hours = 0;
+            }
+
+            Console.WriteLine($"{hours}:{minutes:D2}");
+        }
+    }
+}
