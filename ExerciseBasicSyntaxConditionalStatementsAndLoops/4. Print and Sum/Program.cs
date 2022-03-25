8feeb29fa7d358ec83c3715157e376631f1af4f4
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-03-25 22:41:37 +0200

    Add files via upload

---
+++ ExerciseBasicSyntaxConditionalStatementsAndLoops/4. Print and Sum/Program.cs
@@ -0,0 +1,29 @@
+﻿    using System;
+
+    namespace _4._Print_and_Sum
+    {
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                //Intput
+                int startNum = int.Parse(Console.ReadLine());
+                int endNum = int.Parse(Console.ReadLine());
+
+                int sum = 0;
+                int num = 0;
+
+                //Action
+                for (int i = startNum; i <= endNum; i++)
+                {
+                    sum += i;
+                    Console.Write($"{i} ");
+                }
+
+
+                //Output
+                Console.WriteLine();  
+                Console.WriteLine($"Sum: {sum}");
+            }
+        }
+    }
