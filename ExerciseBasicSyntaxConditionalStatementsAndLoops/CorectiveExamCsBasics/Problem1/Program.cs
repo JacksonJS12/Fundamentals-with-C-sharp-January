3494130af41e6053a1263189c80b366f52695b0f
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-03-25 22:20:43 +0200

    Add files via upload

---
+++ ExerciseBasicSyntaxConditionalStatementsAndLoops/CorectiveExamCsBasics/Problem1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Problem1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double averageSpeed = double.Parse(Console.ReadLine());
+            double rocketFuelPer100km = double.Parse(Console.ReadLine());
+
+            double totalDistance = 384400 * 2;
+            double totalTime = Math.Ceiling(totalDistance / averageSpeed) + 3;
+            double totalRocketFuel = (rocketFuelPer100km * totalDistance) / 100;
+
+            Console.WriteLine(totalTime);
+            Console.WriteLine(totalRocketFuel);
+
+
+        }
+    }
+}
