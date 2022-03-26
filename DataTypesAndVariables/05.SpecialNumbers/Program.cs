1e8c4717d2d10c872a7f2226c60489d5d83a15c3
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-03-26 17:26:12 +0200

    Add files via upload

---
+++ DataTypesAndVariables/05.SpecialNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _05._Special_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= num; i++)
+            {
+                int numToProcess = i;
+                int sum = 0;
+
+                while (numToProcess != 0)
+                {
+                    int currentDigit = numToProcess % 10;
+                    numToProcess /= 10;
+                    sum += currentDigit;
+                }
+                bool isSpecial = sum == 5  || sum == 7 || sum == 11 ;
+
+                
+
+                Console.WriteLine($"{i} -> {isSpecial}");
+            }
+        }
+    }
+}
