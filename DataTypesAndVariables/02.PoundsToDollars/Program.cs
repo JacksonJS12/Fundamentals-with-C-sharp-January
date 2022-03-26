b817aec45eded74310fc65edd5b8aa0738db5682
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-03-26 17:22:18 +0200

    Add files via upload

---
+++ DataTypesAndVariables/02.PoundsToDollars/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _02._Pounds_to_Dollars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var britishPound = double.Parse(Console.ReadLine());
+            decimal usd = (decimal)britishPound * 1.31M;
+            Console.WriteLine($"{usd:f3}");
+        }
+    }
+}
