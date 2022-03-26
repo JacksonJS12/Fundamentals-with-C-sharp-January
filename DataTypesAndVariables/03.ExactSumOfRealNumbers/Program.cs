a0c43d27788e77db4420f7e5815892315b82ce27
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-03-26 17:23:07 +0200

    Add files via upload

---
+++ DataTypesAndVariables/03.ExactSumOfRealNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _03._Exact_Sum_of_Real_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal num = decimal.Parse(Console.ReadLine());
+            decimal sum = 0m;
+
+            for (int i = 1; i <= num; i++)
+            {
+                sum += decimal.Parse(Console.ReadLine());
+            }
+            Console.WriteLine($"{sum}");
+            
+        }
+    }
+}
