26e737a344d0b5514dd51a74baa745ab8fe9bbe7
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-03-26 17:21:43 +0200

    Add files via upload

---
+++ DataTypesAndVariables/01.ConvertMetersToKilometers/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _01._Convert_Meters_to_Kilometers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var meters = int.Parse(Console.ReadLine());
+            decimal kilometers = (decimal)meters / 1000.0M;
+            Console.WriteLine($"{ kilometers:F2}");
+        }
+    }
+}
