ebbe3c9555d7a850794bbef1c633c20769b4ec04
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-03-26 17:28:47 +0200

    Add files via upload

---
+++ DataTypesAndVariables/11.RefactorVolumeOfPyramid/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _11._Refactor_Volume_of_Pyramid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            double length = double.Parse(Console.ReadLine());
+            Console.Write($"Length: ");
+            double widht = double.Parse(Console.ReadLine());
+            Console.Write($"Width: ");
+            double height = double.Parse(Console.ReadLine());
+            Console.Write($"Height: ");
+            double volume = (length * widht * height) / 3;
+            Console.WriteLine($"Pyramid Volume: {volume:f2}");
+
+        }
+    }
+}
