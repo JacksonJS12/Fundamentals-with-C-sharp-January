ab50de0a5e0650b525a9cc371a2c89470553903f
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-03-26 17:27:58 +0200

    Add files via upload

---
+++ DataTypesAndVariables/10.LowerOrUpper/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _10._Lower_or_Upper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char ch = char.Parse(Console.ReadLine());
+
+            bool isUpper = Char.IsUpper(ch);
+
+            if (isUpper != true)
+            {
+                Console.WriteLine("lower-case");
+            }
+            else
+            {
+                Console.WriteLine("upper-case");
+            }
+        }
+    }
+}
