266871ceb33fe0dfc9c59fca64dc7210f986d3ef
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-03-26 17:29:19 +0200

    Add files via upload

---
+++ DataTypesAndVariables/12.RefactorSpecialNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _12._Refactor_Special_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = 1; i <= input; i++)
+            {
+                int num = i;
+                sum = 0;
+                while (num != 0)
+                {
+                    int slenderness = num % 10;
+                    num /= 10;
+                    sum += slenderness;
+                }
+                bool isValid = (sum == 5) || (sum == 7) || (sum == 11);
+
+                Console.WriteLine($"{i} -> {isValid}");
+
+            }
+
+
+        }
+    }
+}
