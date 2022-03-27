f1a379e761542d8efa587447c23673a749661dcc
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-03-27 23:31:59 +0300

    Add files via upload

---
+++ ExerciseDataTypesAndVariables/P11.Snowballs/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Numerics;
+
+namespace _11._Snowballs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int snowballCount = int.Parse(Console.ReadLine());
+
+            int bestSnowballSnow = 0;
+            int bestSnowballTime = 0;
+            int bestSnowballQuality = 0;
+            BigInteger bestSnowballValue = BigInteger.Zero;
+
+            for (int i = 0; i < snowballCount; i++)
+            {
+                int snowballSnow = int.Parse(Console.ReadLine());
+                int snowballTime = int.Parse(Console.ReadLine());
+                int snowballQuality = int.Parse(Console.ReadLine());
+
+                int snowballDevide = snowballSnow / snowballTime;
+                BigInteger snowballValue = BigInteger.Pow(snowballDevide, snowballQuality);
+
+                if (snowballValue >= bestSnowballValue)
+                {
+                    bestSnowballSnow = snowballSnow;
+                    bestSnowballTime = snowballTime;
+                    bestSnowballQuality = snowballQuality;
+                    bestSnowballValue = snowballValue;
+                }
+
+            }
+            Console.WriteLine($"{bestSnowballSnow} : {bestSnowballTime} = {bestSnowballValue} ({bestSnowballQuality})");
+        }
+    }
+}
