68841cb9ad155e3f3926cb62ef90e64fd7b3add7
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-03-27 23:31:32 +0300

    Add files via upload

---
+++ ExerciseDataTypesAndVariables/P10.Pokemon/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+
+namespace _10._Pokemon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
