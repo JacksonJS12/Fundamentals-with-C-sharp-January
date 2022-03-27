4923aa4dd559aedbc774efc3e77f233000d68b8a
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-03-27 23:25:02 +0300

    Add files via upload

---
+++ ExerciseDataTypesAndVariables/P02.SumDigits/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _02._Sum_Digits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+           
+            
+
+            int sum = 0;
+
+            for (int i = num % 10; i != 0; i-- )
+            {
+               sum += num 
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
