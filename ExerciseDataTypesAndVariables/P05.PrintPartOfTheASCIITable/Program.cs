df4da8da29f47a8d6bc6cce2e2bd1d96c9ee9647
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-03-27 23:29:10 +0300

    Add files via upload

---
+++ ExerciseDataTypesAndVariables/P05.PrintPartOfTheASCIITable/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _05._Print_Part_of_the_ASCII_Table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int startIndex = int.Parse(Console.ReadLine());
+            int endIndex = int.Parse(Console.ReadLine());
+
+            for (int currIndex = startIndex; currIndex <= endIndex; currIndex++)
+            {
+                char currCh = (char)currIndex;
+
+                Console.Write($"{currCh} ");
+            }
+            
+        }
+    }
+}
