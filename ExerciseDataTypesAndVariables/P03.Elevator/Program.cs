b73eabc0bb53254f2a1d9933f56f64ce64909327
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-03-27 23:27:56 +0300

    Add files via upload

---
+++ ExerciseDataTypesAndVariables/P03.Elevator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _03._Elevator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int capacity = int.Parse(Console.ReadLine());
+
+            int numOfFullCourse = 0;
+
+            if (num % capacity == 0)
+            {
+                numOfFullCourse = num / capacity;
+
+            }
+            else
+            {
+                numOfFullCourse = num % capacity;
+
+            }
+        }
+    }
+} 
