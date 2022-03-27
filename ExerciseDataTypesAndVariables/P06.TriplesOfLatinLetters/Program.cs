2c4a033d76d7c861c3f13311d4b35c9ba8174964
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-03-27 23:29:42 +0300

    Add files via upload

---
+++ ExerciseDataTypesAndVariables/P06.TriplesOfLatinLetters/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _06._Triples_of_Latin_Letters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
