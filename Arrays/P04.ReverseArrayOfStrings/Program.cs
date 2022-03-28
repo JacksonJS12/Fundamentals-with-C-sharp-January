7300c7b13a74239fe66563e126fcbff3285727b3
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-03-28 22:00:46 +0300

    Add files via upload

---
+++ Arrays/P04.ReverseArrayOfStrings/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _04._Reverse_Array_of_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] items = Console.ReadLine().Split();
+
+            Array.Reverse(items);
+
+            Console.WriteLine(string.Join(" ", items));
+        }
+    }
+}
