a206dd2cbc246490092c2bb718ebd1f31b519552
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-03-28 21:59:39 +0300

    Add files via upload

---
+++ Arrays/P02.PrintNumbersInReverseOrder/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _02._Print_Numbers_in_Reverse_Order
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOfNumbers = int.Parse(Console.ReadLine());
+            int[] items = new int[numOfNumbers];
+
+            for (int i = 0; i < numOfNumbers; i++)
+            {
+                items[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = items.Length - 1; i >= 0; i--)
+            {
+                Console.Write($"{items[i]} ");
+            }
+        }
+    }
+}
