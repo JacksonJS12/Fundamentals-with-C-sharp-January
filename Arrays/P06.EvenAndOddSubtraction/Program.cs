b5242950ca8cfe4541c290006a55af28e5379085
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-03-28 22:02:41 +0300

    Add files via upload

---
+++ Arrays/P06.EvenAndOddSubtraction/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _06._Even_and_Odd_Subtraction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] rawInput = Console.ReadLine().Split();
+            int[] items = new int[rawInput.Length];
+
+            for (int i = 0; i < rawInput.Length; i++)
+            {
+                items[i] = int.Parse(rawInput[i]);
+            }
+
+            int evenSum = 0;
+            int oddSum = 0;
+
+            foreach (int item in items)
+            {
+                if (item % 2 == 0)
+                {
+                    evenSum += item;
+                }
+                else
+                {
+                    oddSum += item;
+                }
+            }
+            Console.WriteLine(evenSum - oddSum);
+        }
+    }
+}
