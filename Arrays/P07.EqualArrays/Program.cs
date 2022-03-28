8fd3fc2e292307bc73a4f88b782d898202a06194
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-03-28 22:03:33 +0300

    Add files via upload

---
+++ Arrays/P07.EqualArrays/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace _07._Equal_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] firstArray = Console.ReadLine()
+                    .Split(' ')
+                    .Select(int.Parse)
+                    .ToArray();
+
+            int[] secondArray = Console.ReadLine()
+                    .Split(' ')
+                    .Select(int.Parse)
+                    .ToArray();
+
+            int sum = 0;
+            bool arraysAreDiffrent = false;
+
+            for (int i = 0; i < firstArray.Length; i++)
+            {
+                sum += secondArray[i];
+                if (firstArray[i] != secondArray[i])
+                {
+                    Console.WriteLine($"Arrays are not identical. Found difference at {i} index");
+                    arraysAreDiffrent = true;
+                    return;
+                    
+                }
+            }
+            if (!arraysAreDiffrent)
+            {
+            Console.WriteLine($"Arrays are identical. Sum: {sum}");
+
+            }
+        }
+    }
+}
