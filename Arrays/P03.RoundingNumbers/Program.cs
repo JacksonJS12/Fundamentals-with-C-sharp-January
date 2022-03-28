0a0c85fcc733ad9d0c4f14733cf68205239ed233
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-03-28 22:00:12 +0300

    Add files via upload

---
+++ Arrays/P03.RoundingNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _03._Rounding_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] rawInput = Console.ReadLine().Split();
+            double[] items = new double[rawInput.Length];
+
+            for (int i = 0; i < rawInput.Length; i++)
+            {
+                items[i] = double.Parse(rawInput[i]);
+            }
+
+            for (int i = 0; i < items.Length; i++)
+            {
+                Console.WriteLine($"{items[i]} => {Math.Round(items[i], MidpointRounding.AwayFromZero)}");
+            }
+        }
+    }
+}
