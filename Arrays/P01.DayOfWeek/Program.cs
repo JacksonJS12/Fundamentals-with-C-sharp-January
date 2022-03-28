5a72246e615b331c6d8e37ba3198a8b510932b9d
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-03-28 21:59:09 +0300

    Add files via upload

---
+++ Arrays/P01.DayOfWeek/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _01._Day_of_Week
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int dayNum = int.Parse(Console.ReadLine());
+
+            string[] daysOfWeek =
+            {
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday"
+
+            };
+
+            if (dayNum >= 1 && dayNum <= daysOfWeek.Length)
+            {
+                Console.WriteLine(daysOfWeek[dayNum-1]);
+            }
+            else
+            {
+                Console.WriteLine("Invalid day!");
+            }
+        }
+    }
+}
