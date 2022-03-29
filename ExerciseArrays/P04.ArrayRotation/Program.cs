e02f6ae6c75a3b59bbadd81c08a66980327fe2c2
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-03-29 22:38:10 +0300

    Add files via upload

---
+++ ExerciseArrays/P04.ArrayRotation/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.ArrayRotation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int rotationsCount = int.Parse(Console.ReadLine());
+
+            for (int rot = 1; rot <= rotationsCount; rot++)
+            {
+                int firstEl = arr[0];
+                for (int i = 0; i <= arr.Length - 2; i++)
+                {
+                    arr[i] = arr[i + 1];
+                }
+                arr[arr.Length - 1] = firstEl;
+
+            }
+
+            Console.WriteLine(String.Join(" ", arr));
+        }
+    }
+}
