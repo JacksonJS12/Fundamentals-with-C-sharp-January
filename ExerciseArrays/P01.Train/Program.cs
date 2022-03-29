90798b9f0d392aa321be023929535d28f2d8c560
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-03-29 22:36:07 +0300

    Add files via upload

---
+++ ExerciseArrays/P01.Train/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.Train
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int wagons = int.Parse(Console.ReadLine());
+            int[] passengers = new int[wagons];
+
+            int sumOfPassengersInWagon = 0;
+
+            int i;
+            for (i = 0; i < wagons; i++)
+            {
+                passengers[i] = int.Parse(Console.ReadLine());
+                sumOfPassengersInWagon += passengers[i];
+
+            }
+
+            for (int j = wagons; j <= passengers.Length; j++)
+            {
+
+                Console.Write(String.Join(' ', passengers));
+                Console.WriteLine();
+            }
+            Console.WriteLine(sumOfPassengersInWagon);
+        }
+    }
+}
