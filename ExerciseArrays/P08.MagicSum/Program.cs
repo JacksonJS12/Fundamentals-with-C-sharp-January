003a8985a55a3c9006f3e4ee774bc1cacd731b21
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-03-29 22:40:38 +0300

    Add files via upload

---
+++ ExerciseArrays/P08.MagicSum/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace _08.MagicSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int targetNum = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    if (numbers[i] + numbers[j] == targetNum)
+                    {
+
+                        Console.WriteLine($"{numbers[i]} {numbers[j]}");
+
+
+                    }
+
+                }
+
+            }
+
+        }
+    }
+}
