25abdab2133e00b42c2dc145aaa03450e376f6e8
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-03-29 22:40:03 +0300

    Add files via upload

---
+++ ExerciseArrays/P07.MaxSequenceOfEqualElements/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07.Max_SequenceOfEqualElements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            for (int i = 0; i < numbers.Length; i++)
+            { 
+            Console.WriteLine(string.Join(" ", numbers.Where(x => numbers[i] == numbers[i + 1])));
+            }
+        }
+    }
+}
