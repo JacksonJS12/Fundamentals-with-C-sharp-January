5bc7eb7ae4522e4037ab7114cbcd1f669d3086a1
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-03-29 22:36:48 +0300

    Add files via upload

---
+++ ExerciseArrays/P02.CommonElements/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.CommonElements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] firstElements = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            string[] secondElements = Console.ReadLine()
+               .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+               .ToArray();
+            for (int i = 0; i < secondElements.Length; i++)
+            {
+                foreach (string item in firstElements)
+                {
+                    if (item.Contains(secondElements[i]))
+                    {
+                        Console.Write($"{item} ");
+                    }
+                }
+
+            }
+
+
+        }
+
+    }
+}
