246769aae9465b533889e5df148fa37c8c16a7fd
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-03-29 22:39:29 +0300

    Add files via upload

---
+++ ExerciseArrays/P06.EqualSums/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+
+namespace _06.EqualSums
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ia = Console.ReadLine()
+                .Split(' ')
+                .Where(x => x.Length > 0)
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] LeftToRightSum = new int[ia.Length];
+            int[] RightToLeftSum = new int[ia.Length];
+
+            int T = 0;
+
+            for (int i = 0; i < ia.Length; i++)
+            {
+                T += ia[i];
+                LeftToRightSum[i] = T;
+            }
+
+            T = 0;
+
+            for (int i = ia.Length - 1; i >= 0; i--)
+            {
+                T += ia[i];
+                RightToLeftSum[i] = T;
+            }
+
+            int ix = -1;
+
+            for (int i = 0; i < ia.Length; i++)
+            {
+                if (LeftToRightSum[i] == RightToLeftSum[i])
+                {
+                    ix = i;
+                    break;
+                }
+            }
+
+            if (ix < 0)
+            {
+                Console.WriteLine("no");
+            }
+            else
+            {
+                Console.WriteLine(ix);
+            }
+        }
+    }
+}
