91729742e0e14768d6a43d76d1b34152396a87ae
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-03-30 14:03:58 +0300

    Add files via upload

---
+++ Methods/P11.MathOperations/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace P11.MathOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            char @operator = char.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+
+            Result(firstNum, secondNum, @operator);
+        }
+
+        static int Result(int firstNum, int secondNum, char @operator)
+        {
+            int result = 0;
+
+            if (@operator == '+')
+            {
+                result = firstNum + secondNum;
+            }
+            else if (@operator == '/')
+            {
+                result = firstNum / secondNum;
+            }
+            else if (@operator == '-')
+            {
+                result = firstNum - secondNum;
+            }
+            else if (@operator == '*')
+            {
+                result = firstNum * secondNum;
+            }
+
+            Console.WriteLine(result);
+            return result;
+
+        }
+    }
+}
