abe692d88f7f2e08dd4bdd1e44778b29583970be
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-03-30 14:03:45 +0300

    Add files via upload

---
+++ Methods/P10.MultiplyEvensByOdds/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace P10.MultiplyEvensByOdds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Math.Abs(int.Parse(Console.ReadLine()));
+            var sumOfEvenDigits = GetSumOfEvenDigits(number);
+            var sumOfOddDigits = GetSumOfOddDigits(number);
+            Console.WriteLine(sumOfEvenDigits * sumOfOddDigits);
+        }
+
+        static int GetSumOfEvenDigits(int number)
+        {
+            int sum = 0;
+
+            while (number != 0)
+            {
+                int digit = number % 10;
+                if (digit % 2 == 0)
+                {
+                    sum += number % 10;
+
+                }
+                number /= 10;
+            }
+            return sum;
+        }
+
+        static int GetSumOfOddDigits(int number)
+        {
+            int sum = 0;
+
+            while (number != 0)
+            {
+                int digit = number % 10;
+                if (digit % 2 != 0)
+                {
+                    sum += number % 10;
+
+                }
+                number /= 10;
+            }
+            return sum;
+        }
+    }
+}
