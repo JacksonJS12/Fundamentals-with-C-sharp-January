d0398971be9ee10ae75335975cee16d1150bfb72
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-03-30 14:01:08 +0300

    Add files via upload

---
+++ Methods/P01.SignOfIntegerNumbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace P01.SignOfIntegerNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            PrintNumberSigh(number);
+        }
+
+        static void PrintNumberSigh(int number)
+        {
+            if (number > 0)
+            {
+                Console.WriteLine($"The number {number} is positive. ");
+            }
+            else if(number < 0)
+            {
+                Console.WriteLine($"The number {number} is negative. ");
+            }
+            else
+            {
+                Console.WriteLine($"The number {number} is zero. ");
+            }
+        }
+    }
+}
