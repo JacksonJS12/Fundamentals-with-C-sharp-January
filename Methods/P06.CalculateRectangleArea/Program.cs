1d89a24e35ca8c19344a40c2c17ff4beaa641667
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-03-30 14:02:21 +0300

    Add files via upload

---
+++ Methods/P06.CalculateRectangleArea/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace P06.CalculateRectangleArea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+            double area = GetRectangleArea(width, height);
+            Console.Write(area);
+        }
+
+        static double GetRectangleArea(double width, double height)
+        {
+            return width * height;
+        }
+    }
+}
