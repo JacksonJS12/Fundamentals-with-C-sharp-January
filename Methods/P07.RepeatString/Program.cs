cf31bd5a8fd51289d815c2775a66ff1274129bd2
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-03-30 14:02:33 +0300

    Add files via upload

---
+++ Methods/P07.RepeatString/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace P07.RepeatString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string template = Console.ReadLine();
+            int repeatTimes = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(RepeatString(template, repeatTimes));
+            
+        }
+
+        static string RepeatString(string template, int numOfRepates)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            for (int i = 0; i < numOfRepates; i++)
+            {
+                stringBuilder.Append(template);
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+}
