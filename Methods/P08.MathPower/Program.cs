f68ae57a78870b0c8aef613fff3029be1f965ec7
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-03-30 14:03:03 +0300

    Add files via upload

---
+++ Methods/P08.MathPower/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace P08.MathPower
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double @base = double.Parse(Console.ReadLine());
+            double power = double.Parse(Console.ReadLine());
+            double powerReault = MathPower(@base, power);
+            Console.WriteLine(powerReault);
+        }
+
+        static double MathPower(double @base, double power)
+        {
+            double result = 1;
+
+            for (int i = 0; i < power; i++)
+            {
+                result *= @base;
+            }
+
+            return result;
+        }
+    }
+}
