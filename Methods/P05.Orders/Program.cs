220201322bc4dfc6260c335686f609aad4c281ac
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-03-30 14:02:10 +0300

    Add files via upload

---
+++ Methods/P05.Orders/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace P05.Orders
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string product = Console.ReadLine();
+            int quantity = int.Parse(Console.ReadLine());
+
+            TotalOrderPrice(product, quantity);
+
+        }
+
+        static void TotalOrderPrice(string product, int quantity)
+        {
+            decimal price = 0m;
+            if (product == "coffee")
+            {
+                price = 1.50m;
+            }
+            else if (product == "water")
+            {
+                price = 1.00m;
+            }
+            else if (product == "coke")
+            {
+                price = 1.40m;
+            }
+            else if (product == "snacks")
+            {
+                price = 2.00m;
+            }
+
+
+            Console.WriteLine($"{quantity * price:f2}");
+        }
+
+
+    }
+}
