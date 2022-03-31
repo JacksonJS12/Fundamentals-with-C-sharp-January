1e7e1fb113534b428858cfcdd75b3c70b8b8c874
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-03-31 20:21:59 +0300

    Add files via upload

---
+++ ExerciseMethods/P08.FactorialDivision/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace P08.FactorialDivision
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double numberOne = double.Parse(Console.ReadLine());
+            double numberTwo = double.Parse(Console.ReadLine());
+
+
+            double total = Factorial(numberOne) / Factorial(numberTwo);
+            Console.WriteLine($"{total:f2}");
+        }
+        static double Factorial(double number)
+        {
+            double factorialNumSum = 1;
+
+            
+                for (double num1 = 1; num1 <= number; num1++)
+                {
+                    factorialNumSum *= num1;
+                }
+
+                return factorialNumSum;
+            
+        }
+    }
+}
