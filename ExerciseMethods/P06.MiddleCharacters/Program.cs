d69ff5fdea5154c2d775a12ec384b97a3ce5451b
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-03-31 20:21:19 +0300

    Add files via upload

---
+++ ExerciseMethods/P06.MiddleCharacters/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace P06.MiddleCharacters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            MiddleCharecter(input);
+        }
+
+        static void MiddleCharecter(string input)
+        {
+            int length = input.Length;
+            if (length % 2 != 0)
+            {
+                Console.WriteLine(input[length/2]);
+            }
+            else
+            {
+                length = length / 2;
+                Console.WriteLine(input.Substring((length) - 1, 2));
+              
+            }
+        }
+    }
+}
