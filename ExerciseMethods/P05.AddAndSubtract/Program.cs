a06c65fc66ca70bd193d1f20a19eda5f15b7f4bf
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-03-31 20:20:58 +0300

    Add files via upload

---
+++ ExerciseMethods/P05.AddAndSubtract/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace P05.AddAndSubtract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            int tirthNum = int.Parse(Console.ReadLine());
+
+            TotalSum(firstNum, secondNum, tirthNum);
+        }
+
+        static int IntegerGathering(int firstNum, int secondNum)
+        {
+           int firstPlusSecond = firstNum + secondNum;
+
+            return firstPlusSecond;
+        }
+
+        static void TotalSum(int firstNum, int secondNum, int tirthNum)
+        {
+            Console.WriteLine(IntegerGathering(firstNum, secondNum) - tirthNum);
+        }
+    }
+}
