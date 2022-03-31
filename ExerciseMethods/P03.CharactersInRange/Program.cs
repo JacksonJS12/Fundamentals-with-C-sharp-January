c26892207ec8c8b1405be92a9c0f12806ee443ec
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-03-31 20:19:59 +0300

    Add files via upload

---
+++ ExerciseMethods/P03.CharactersInRange/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace P03.CharactersInRange
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstObj = char.Parse(Console.ReadLine());
+            char secondObj = char.Parse(Console.ReadLine());
+
+
+            AsciiTable1(firstObj, secondObj);
+
+            AsciiTable2(firstObj, secondObj);
+        }
+
+        static void AsciiTable1(char firstObj, char secondObj)
+        {
+            int charIndex1 = (int)firstObj;
+            int charIndex2 = (int)secondObj;
+
+
+            for (int i = charIndex1 + 1; i < charIndex2; i++)
+            {
+                Console.Write($"{(char)i} ");
+            }
+
+
+
+
+        }
+        static void AsciiTable2(char firstObj, char secondObj)
+        {
+            int charIndex1 = (int)firstObj;
+            int charIndex2 = (int)secondObj;
+
+            for (int i = charIndex2 + 1; i < charIndex1; i++)
+            {
+                Console.Write($"{(char)i} ");
+            }
+        }
+    }
+}
+
