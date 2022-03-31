9878a678b8ad0a0736a974aae00451f7aaa3dd6a
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-03-31 20:22:19 +0300

    Add files via upload

---
+++ ExerciseMethods/P09.PalindromeIntegers/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace P09.PalindromeIntegers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string command = Console.ReadLine();
+            PalindromeInteger(command);
+
+
+        }
+
+        static void PalindromeInteger(string command)
+        {
+            while (command != "END")
+            {
+                int palindromeInteger = 0;
+                int reverseNumber = 0;
+                int sum = 0;
+                int temporaryVariable;
+                
+                palindromeInteger = int.Parse(command);
+                temporaryVariable = palindromeInteger;
+                while (palindromeInteger > 0)
+                {
+                    reverseNumber = palindromeInteger % 10;
+                    sum = (sum * 10) + reverseNumber;
+                    palindromeInteger = palindromeInteger / 10;
+                }
+
+                if (temporaryVariable == sum)
+                {
+
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    Console.WriteLine("false");
+                }
+
+                command = Console.ReadLine();
+
+            }
+
+
+            
+        }
+    }
+
+}
