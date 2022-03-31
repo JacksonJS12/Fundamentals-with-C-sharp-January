48ae73866b506f0f9c99c1e024dae4bf9bad61cf
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-03-31 20:21:40 +0300

    Add files via upload

---
+++ ExerciseMethods/P07.NxNMatrix/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace P07.NxNMatrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            NxN(number);
+        }
+
+        static void NxN(int number)
+        {
+            for (int rows = 0; rows < number; rows++)
+            {
+                for (int col = 1; col < number; col++)
+                {
+                    Console.Write($"{number} ");
+                }
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
