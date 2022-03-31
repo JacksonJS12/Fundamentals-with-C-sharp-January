78dada342eada46e39de6e3e821eaaacc0f7d112
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-03-31 20:19:28 +0300

    Add files via upload

---
+++ ExerciseMethods/P02.VowelsCount/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace P02.VowelsCount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+
+            int vowelsCount = GetVowelsCount(word);
+
+            Console.WriteLine(vowelsCount);
+        }
+
+        static int GetVowelsCount(string word)
+        {
+            char[] vowels = new char[]
+            {
+                'a',
+                'e',
+                'i',
+                'o',
+                'u'
+            };
+            int vowelsCount = 0;
+            foreach (char ch in word.ToLower())
+            {
+                if (vowels.Contains(ch))
+                {
+                    vowelsCount++;
+                }
+            }
+
+            return vowelsCount;
+        }
+    }
+}
