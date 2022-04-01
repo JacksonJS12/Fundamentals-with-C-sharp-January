f346355ea7aee89cef7a9d11d0b95acd6c9e810a
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-04-01 14:46:37 +0300

    Add files via upload

---
+++ List/P04.ListOfProducts/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P04.ListOfProducts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOfProducts = int.Parse(Console.ReadLine());
+            List<String> products = new List<String>();
+
+            for (int i = 0; i < numOfProducts; i++)
+            {
+                string product = Console.ReadLine();
+                products.Add(product);
+            }
+
+            products.Sort();
+
+            for (int i = 0; i < products.Count; i++)
+            { 
+                Console.WriteLine($"{i + 1}.{products[i]}");
+            }
+        }
+    }
+}
