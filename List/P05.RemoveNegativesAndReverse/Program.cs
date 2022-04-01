6d91a12e3d1d3406b6bf31aa29615f800d6d4995
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-04-01 14:46:49 +0300

    Add files via upload

---
+++ List/P05.RemoveNegativesAndReverse/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P05.RemoveNegativesAndReverse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            numbers.RemoveAll(x => x < 0);
+            numbers.Reverse();
+
+            if (numbers.Count == 0)
+            {
+                Console.WriteLine("empty");
+            }
+
+            else
+            {
+            Console.WriteLine(String.Join(" ", numbers)); 
+
+            }
+        }
+    }
+}
