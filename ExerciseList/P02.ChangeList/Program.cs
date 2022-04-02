8565408bce7c9622b45eb05cde15e01f328cab46
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-04-02 18:03:14 +0300

    Add files via upload

---
+++ ExerciseList/P02.ChangeList/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P02.ChangeList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            string command = Console.ReadLine();
+
+            while (command != "end")
+            {
+                string[] commandArgs = command
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                string commandType = commandArgs[0];
+                string element = commandArgs[1];
+
+                if (commandType == "Delete")
+                {
+                    numbers.RemoveAll(x => x == int.Parse(element));
+                }
+
+                if (commandType == "Insert")
+                {
+                    string position = commandArgs[2];
+                    numbers.Insert(int.Parse(position), int.Parse(element));
+                }
+
+                command = Console.ReadLine();
+            }
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
