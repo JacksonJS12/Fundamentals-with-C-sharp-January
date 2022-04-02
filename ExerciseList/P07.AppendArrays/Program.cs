c9c8e0a3bec377d2a8b5b1e61248f3e13d6d20cd
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-04-02 18:06:28 +0300

    Add files via upload

---
+++ ExerciseList/P07.AppendArrays/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P07.AppendArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> list = Console.ReadLine()
+                .Split('|', StringSplitOptions.RemoveEmptyEntries)
+                .Reverse()
+                .ToList();
+            List<int> array = new List<int>();  
+            foreach (var arr in list)
+            {
+                array.AddRange(arr.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                        .Select(int.Parse)
+                        .ToList()
+                         );
+            }
+            Console.WriteLine(string.Join(" ", array));
+        }
+    }
+}
