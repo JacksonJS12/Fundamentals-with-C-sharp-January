b3a1d6b1fe3a04a3dd1fb5beeb91de04dafefb97
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-04-02 18:04:21 +0300

    Add files via upload

---
+++ ExerciseList/P05.BombNumbers/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P05.BombNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            int[] bombInfo = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int bombNum = bombInfo[0];
+            int bombPower = bombInfo[1];
+
+            //int indexOfBomb;
+            //while ((indexOfBomb = numbers.IndexOf(bombNum)) >= 0)
+            //{
+
+            //}
+
+            while (true)
+            {
+                int indexOfBomb = numbers.IndexOf(bombNum);
+
+                if (indexOfBomb == -1)
+                {
+                    break;
+                }
+                DetonateBomb(numbers, indexOfBomb, bombPower);
+            }
+            Console.WriteLine(numbers.Sum());
+            
+        }
+
+        static void DetonateBomb(List<int> numbers, int indexOfBomb, int bombPower)
+        {
+            int rightIndex = indexOfBomb + bombPower;
+            for (int counter = indexOfBomb; counter <= rightIndex; counter++)
+            {
+                if (indexOfBomb >= numbers.Count)
+                {
+                    break;
+                }
+
+                numbers.RemoveAt(indexOfBomb);
+            }
+
+            int leftCount = indexOfBomb - bombPower;
+            if (leftCount < 0)
+            {
+                leftCount = 0;
+            }
+            for (int cnt = leftCount; cnt < indexOfBomb; cnt++)
+            {
+                if (leftCount >= numbers.Count)
+                {
+                    break;
+                }
+                numbers.RemoveAt(leftCount);
+            }
+        }
+    }
+}
