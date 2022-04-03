88de75182f8910102d27b9b1a220dbd7cb23c891
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-04-03 23:50:59 +0300

    Add files via upload

---
+++ ObjectsAndClasses/P01.RandomizeWords/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace P01.RandomizeWords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            Random random = new Random();
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                int randomIndex = random.Next(0, words.Length);
+
+                string currentWord = words[i];
+                words[i] = words[randomIndex];
+                words[randomIndex] = currentWord;
+            }
+
+            foreach (var word in words)
+            {
+                Console.WriteLine(word);
+            }
+           
+        }
+    }
+}
