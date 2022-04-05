1f7c137a1075e679810880c44064665b739ce038
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-04-05 18:36:28 +0300

    Add files via upload

---
+++ AssociativeArrays/P02.OddOccurrences/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P02.OddOccurrences
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine()
+                .Split(' ',StringSplitOptions.RemoveEmptyEntries);
+
+            Dictionary<string, int> occurencies = new Dictionary<string, int>();
+
+            foreach (string word in words)
+            {
+                string loweCaseWord = word.ToLower();
+
+                if (occurencies.ContainsKey(loweCaseWord))
+                {
+                    occurencies[loweCaseWord] += 1;
+                }
+                else
+                {
+                    occurencies.Add(loweCaseWord, 1);
+                }
+            }
+           string[] result = occurencies.Where(item => item.Value % 2 != 0)
+                .Select(item => item.Key)
+                .ToArray();
+
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
