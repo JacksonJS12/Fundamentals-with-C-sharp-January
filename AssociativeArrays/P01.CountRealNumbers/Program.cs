8e37a1f0c42c010503e17f740bb58a3f1158140e
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-04-05 18:36:06 +0300

    Add files via upload

---
+++ AssociativeArrays/P01.CountRealNumbers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P01.CountRealNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            SortedDictionary<int, int> occurencies = new SortedDictionary<int, int>();
+
+            foreach (int number in numbers)
+            {
+                if (occurencies.ContainsKey(number))
+                {
+                    occurencies[number] += 1;
+                }
+                else
+                {
+                    occurencies.Add(number, 1);
+                }
+            }
+            foreach (var item in occurencies)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
