9e449546656c75782419678bfd59bc744641d8c8
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-04-05 18:36:47 +0300

    Add files via upload

---
+++ AssociativeArrays/P03.WordSynonyms/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P03.WordSynonyms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int paiCount = int.Parse(Console.ReadLine());
+
+            Dictionary<string, List<string>> synonimList = new 
+                Dictionary<string, List<string>>();
+
+            for (int i = 0; i < paiCount; i++)
+            {
+                string word = Console.ReadLine();
+                string synonym = Console.ReadLine();
+
+                if (synonimList.ContainsKey(word))
+                {
+                    synonimList[word].Add(synonym);
+
+                }
+                else
+                {
+                    List<string> newSymList = new List<string>();
+                    newSymList.Add(synonym);
+                    synonimList.Add(word, newSymList);
+                }
+            }
+            foreach (var item in synonimList)
+            {
+                Console.WriteLine($"{item.Key} - {string.Join(", ", item.Value)}");
+
+            }
+        }
+    }
+}
