e382c792b40d545adf5a9b2aa1e9a1f560241c67
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-04-06 11:52:44 +0300

    Add files via upload

---
+++ AssociativeArraysExercise/P01.CountCharsInAString/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P01.CountCharsInAString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            Dictionary<char, int> final = new Dictionary<char, int>();
+            foreach (var ch in text)
+            {
+                if (!final.ContainsKey(ch))
+                {
+                    final.Add(ch, 1);
+                }
+                else if (final.ContainsKey(ch))
+                {
+                    final[ch]++;
+                }
+
+            }
+
+            foreach (KeyValuePair<char, int> ch in final.Where(a => a.Key != ' '))
+            {
+                Console.WriteLine($"{ch.Key} -> {ch.Value}");
+            }
+        }
+    }
+}
