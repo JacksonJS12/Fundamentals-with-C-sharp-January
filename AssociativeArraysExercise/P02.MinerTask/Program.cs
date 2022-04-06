63bd5f255d9fa0dc19a730584fad0b1ed92e4f74
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-04-06 11:53:09 +0300

    Add files via upload

---
+++ AssociativeArraysExercise/P02.MinerTask/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P02.MinerTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, long> resourses = new Dictionary<string, long>();
+
+            string resoursesCommand = Console.ReadLine();
+
+            while (resoursesCommand != "stop")
+            {
+                int quantity = int.Parse(Console.ReadLine());
+
+                if (resourses.ContainsKey(resoursesCommand))
+                {
+                    resourses[resoursesCommand] += quantity;
+                }
+                else
+                {
+                    resourses.Add(resoursesCommand, quantity);
+                }
+
+                resoursesCommand = Console.ReadLine();
+            }
+
+            foreach (var resoursesQuantityPair in resourses)
+            {
+                string currentResourse = resoursesQuantityPair.Key;
+                long resourseQuantity = resoursesQuantityPair.Value;
+                Console.WriteLine($"{currentResourse} -> {resourseQuantity}");
+            }
+        }
+    }
+}
