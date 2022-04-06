7522061097b111dd2500e8c02de7148dd1edb99e
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-04-06 11:54:52 +0300

    Add files via upload

---
+++ AssociativeArraysExercise/P08.CompanyUsers/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P08.CompanyUsers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, HashSet<string>> companies = new Dictionary<string, HashSet<string>>();
+
+            string command = Console.ReadLine();
+            while (!command.Contains("End"))
+            {
+                string company = command.Split(" -> ")[0];
+                string employee = command.Split(" -> ")[1];
+
+                if (companies.ContainsKey(company))
+                {
+                    companies[company].Add(employee);
+                }
+                else
+                {
+                    var employees = new HashSet<string>() { employee };
+                    companies.Add(company, employees);
+                }
+
+                command = Console.ReadLine();
+            }
+
+           
+            companies.ToList().ForEach(x =>
+            {
+                Console.WriteLine($"{x.Key}");
+                x.Value.ToList().ForEach(e =>
+                {
+                    Console.WriteLine($"-- {e}");
+                });
+            });
+        }
+    }
+}
