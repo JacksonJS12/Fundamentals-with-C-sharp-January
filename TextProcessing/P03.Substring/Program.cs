2ef0ef48fe6ba6e124137941f9555c46c68afc0a
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-04-07 14:01:08 +0300

    Add files via upload

---
+++ TextProcessing/P03.Substring/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace P03.Substring
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string wordToRemove = Console.ReadLine();
+            string text = Console.ReadLine();
+
+            while (text.Contains(wordToRemove))
+            {
+                text = text.Replace(wordToRemove,"");
+            }
+            Console.WriteLine(text);
+        }
+    }
+}
