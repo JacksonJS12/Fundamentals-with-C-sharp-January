36e22de60f18830a8f4a3f6b25db0b2afe58346e
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-04-07 14:00:07 +0300

    Add files via upload

---
+++ TextProcessing/P01.ReverseStrings/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace P01.ReverseStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            
+            while (command != "end")
+            {
+                string word = command;
+                
+                Console.WriteLine($"{word} = {ReverseString(word)}");
+                command = Console.ReadLine();
+            }
+        }
+
+        static string ReverseString(string word)
+        {
+            char[] chars = word.ToCharArray();
+            string reverse = string.Empty;
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                reverse += word[i];
+            }
+            return reverse;
+        }
+    }
+}
