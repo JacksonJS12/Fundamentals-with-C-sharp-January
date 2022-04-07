d196475acc9f5041bbe21ddf2895047866a1ca85
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-04-07 14:02:01 +0300

    Add files via upload

---
+++ TextProcessing/P05.DigitsLettersAndOther/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P05.DigitsLettersAndOther
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            string digits = new string(text.Where(Char.IsDigit).ToArray());
+
+      
+
+            string letters = new string(text.Where(Char.IsLetter).ToArray());
+
+            Console.WriteLine(digits);
+            Console.WriteLine(letters);
+           
+            for (int i = 0; i < text.Length; i++)
+            {
+                bool isDigitOrLetter = Char.IsLetterOrDigit(text[i]);
+                if (!isDigitOrLetter)
+                {
+                    Console.Write(text[i]);
+                }
+            }
+            
+        }
+    }
+}
