e2a588986865df2bc14ec7a16dcbb2c36c53cc05
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-04-07 14:00:47 +0300

    Add files via upload

---
+++ TextProcessing/P02.RepeatStrings/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace P02.RepeatStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] allWords = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+
+            string result = string.Empty; 
+            foreach (var word in allWords)
+            {
+                for (int i = 0; i < word.Length; i++)
+                {
+                    result += word;
+                }
+            }
+            Console.WriteLine(result); 
+        }
+    }
+}
