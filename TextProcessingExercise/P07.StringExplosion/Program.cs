ac09cf789330fc8492de19bfb3cc08c2ddbf5dab
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-04-08 10:43:48 +0300

    Add files via upload

---
+++ TextProcessingExercise/P07.StringExplosion/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+
+namespace P07.StringExplosion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputString = Console.ReadLine();
+            StringBuilder outputText = new StringBuilder();
+            int bombPower = 0;
+
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                char currCh = inputString[i];
+
+                if (currCh == '>')
+                {
+                    int currBombPower = GetIntValueOfCharacter(inputString[i + 1]);
+
+                    outputText.Append(currCh);
+
+                    bombPower += currBombPower;
+                }
+                else
+                {
+                    if (bombPower > 0)
+                    {
+                        bombPower--;
+                    }
+                    else
+                    {
+                        outputText.Append(currCh);
+                    }
+                }
+            }
+            Console.WriteLine(outputText.ToString());
+
+        }
+
+        static int GetIntValueOfCharacter(char ch)
+        {
+            return (int)ch - 48;
+        }
+    }
+}
