bbf5395977af3e35ec468a9a37b30954ee98a84c
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-04-08 10:42:18 +0300

    Add files via upload

---
+++ TextProcessingExercise/P03.ExtractFile/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace P03.ExtractFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = Console.ReadLine();
+
+           string fileInfo = filePath.Substring(filePath.LastIndexOf('\\') + 1);
+
+            int dotIndex = fileInfo.LastIndexOf('.');
+            string fileName = fileInfo.Substring(0, dotIndex);
+           string fileExtencion = fileInfo.Substring(dotIndex + 1);
+
+            Console.WriteLine($"File name: {fileName}");
+            Console.WriteLine($"File extension: {fileExtencion}");
+        }
+    }
+}
