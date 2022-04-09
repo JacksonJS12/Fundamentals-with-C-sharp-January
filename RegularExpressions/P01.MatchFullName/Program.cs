3c3feeeb1d38747aa135b7b71e2cb41daf62c0c4
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-04-09 11:41:55 +0300

    Add files via upload

---
+++ RegularExpressions/P01.MatchFullName/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace P01.MatchFullName
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string patern = @"\b([A-Z][a-z]{1,}) ([A-Z][a-z]{1,})\b";
+
+            string input = Console.ReadLine();
+
+            MatchCollection matches = Regex.Matches(input, patern);
+
+            foreach (Match item in matches)
+            {
+                Console.Write(item.Value + " ");
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
