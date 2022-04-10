d05e44f82ed42171992683d1289210b8a887b2c9
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-04-10 21:09:11 +0300

    Add files via upload

---
+++ MidExamPreparation/P01.SoftUniReception/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace P01.SoftUniReception
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstEmployee = int.Parse(Console.ReadLine());
+            int secondEmployee = int.Parse(Console.ReadLine());
+            int thirdEmployee = int.Parse(Console.ReadLine());
+            int studentsCount = int.Parse(Console.ReadLine());
+
+            int answeredStudentsPerHour = firstEmployee + secondEmployee + thirdEmployee;
+
+            int hours = 0;
+
+            while (studentsCount >= 0)
+            {
+                if (hours % 4 != 0) // have a break for 1 hour after every 3th hour of work
+                {
+                    hours++;
+
+                }
+                for (int hour = 0; hour < 3; hour++)
+                {
+                    if (studentsCount <= 0)
+                    {
+                        break;
+                    }
+                    studentsCount -= answeredStudentsPerHour;
+                    hours++;
+                }
+                if (studentsCount <= 0)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine($"Time needed: {hours}h.");
+        }
+    }
+}
