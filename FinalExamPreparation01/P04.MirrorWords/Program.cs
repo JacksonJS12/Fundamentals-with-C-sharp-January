858a2363216eace1e377df3476001e349046f4df
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-04-11 22:46:36 +0300

    Add files via upload

---
+++ FinalExamPreparation01/P04.MirrorWords/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace P04.MirrorWords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex pattern = new Regex(@"(#|@)([a-zA-Z]{3,})(\1)(\1)([a-zA-Z]{3,})(\1)");
+
+            string massege = Console.ReadLine();
+            List<string[]> results = new List<string[]>();
+
+            MatchCollection matches = pattern.Matches(massege);
+
+            if (matches.Count == 0)
+            {
+                Console.WriteLine("No word pairs found!");
+            }
+            else
+            {
+                Console.WriteLine($"{matches.Count} word pairs found!");
+            }
+            foreach (Match matche in matches)
+            {
+                string firstWord = matche.Groups[2].Value;
+                string secondWord = matche.Groups[5].Value;
+
+                string reversedSecondWors = string.Join("", secondWord.Reverse());
+
+                if (firstWord == reversedSecondWors)
+                {
+                    results.Add(new string[] { firstWord, secondWord });
+                }
+            }
+
+            if (results.Count == 0)
+            {
+                Console.WriteLine("No mirror words!");
+            }
+            else
+            {
+                string[] messages = results.Select(word => $"{word[0]} <=> {word[1]}")
+                    .ToArray();
+
+                Console.WriteLine($"The mirror words are:");
+                Console.WriteLine(string.Join(", ", messages));
+
+
+            }
+        }
+    }
+}
