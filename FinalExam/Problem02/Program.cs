e2c53d5ee1d50cd22e3acbf67141e470ddebfde4
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-04-13 23:10:39 +0300

    Add files via upload

---
+++ FinalExam/Problem02/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Problem02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"!([A-Z][a-z]{3,})!:\[([A-Za-z]{8,})\]";
+
+            int n = int.Parse(Console.ReadLine());
+            string input = string.Empty;
+            for (int i = 0; i < n; i++)
+            {
+                input = Console.ReadLine();
+
+                Match match = Regex.Match(input, pattern);
+                if (!match.Success)
+                {
+                    Console.WriteLine("The message is invalid");
+                }
+                else
+                {
+                    string command = match.Groups[1].Value;
+                    string number = match.Groups[2].Value;
+
+                    Console.Write($"{command}: ");
+                    foreach (var c in number)
+                    {
+                        Console.Write($"{(int)c} ");
+                    }
+                    Console.WriteLine();
+                }
+                
+            }
+        }
+    }
+}
