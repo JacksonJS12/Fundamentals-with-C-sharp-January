bf7df79589b6dd2f473a695f1e49d0f94bc9921c
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-04-15 12:16:14 +0300

    Add files via upload

---
+++ MidExam/P02.FriendListMaintenance/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.FriendListMaintenance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> usernames = new List<string>();
+            usernames = Console.ReadLine()
+                .Split(',', StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            string command = Console.ReadLine();
+            string[] commandType = new string[command[2]];
+
+            commandType = Console.ReadLine()
+                    .Split(',', StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+            while (command != "Report")
+            {
+
+                if (commandType[0] == "Blacklist")
+                {
+                    Console.WriteLine($"{usernames} was blacklisted");
+                    
+                }
+                if (commandType[0] == "Error")
+                {
+                    Console.WriteLine($"{usernames} was lost due to an error." );
+                }
+                if (commandType[0] == "Change")
+                {
+                    Console.WriteLine($"{usernames} changed his username to {usernames}");
+                }
+
+                command = Console.ReadLine();
+               
+            }
+
+        }
+    }
+}
