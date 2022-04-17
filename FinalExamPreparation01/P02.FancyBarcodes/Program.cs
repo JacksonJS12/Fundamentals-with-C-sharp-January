62aa22c90fedef734b5a2f8ada237882152127f3
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-04-17 21:08:50 +0300

    Add files via upload

---
+++ FinalExamPreparation01/P02.FancyBarcodes/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace P02.FancyBarcodes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Regex pattern = new Regex(@"@#+[A-Z][A-Za-z\d]{4,}[A-Z]@#+");
+            int barcodeCount = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < barcodeCount; i++)
+            {
+                string barcode = Console.ReadLine();
+
+                if (pattern.IsMatch(barcode))
+                {
+                    char[] digits = barcode.Where(x => char.IsDigit(x)).ToArray();
+
+                    if (digits.Length == 0)
+                    {
+                        Console.WriteLine("Product group: 00");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Product group: {string.Join("", digits)}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid barcode");
+                }
+            }
+        }
+    }
+}
