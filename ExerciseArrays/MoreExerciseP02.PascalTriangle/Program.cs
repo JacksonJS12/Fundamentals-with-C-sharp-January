11672f42e20021f83a48c20998eaf28787c32456
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-04-21 22:55:53 +0300

    Add files via upload

---
+++ ExerciseArrays/MoreExerciseP02.PascalTriangle/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace MoreExerciseP02.PascalTriangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+
+            for (int row = 1; row <= rows; row++)
+            {
+                int[] thisRow = new int[row];
+
+                for (int i = 0; i < thisRow.Length; i++)
+                {
+                    if (row != 1)
+                    {
+                        thisRow[i] = thisRow[i] + 
+                    }
+                }
+
+
+
+
+                int[] latRow = thisRow;
+            }
+        }
+    }
+}
