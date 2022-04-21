f0eca93626efdb0786be95ee50f12baa3400d3f7
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-04-21 22:56:07 +0300

    Add files via upload

---
+++ ExerciseArrays/MoreExerciseP03.RecursiveFibonacci/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MoreExerciseP03.RecursiveFibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
