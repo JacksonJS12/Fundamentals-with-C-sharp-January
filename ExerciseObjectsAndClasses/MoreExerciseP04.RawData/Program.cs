32a56abcdec0029bd0296e6fee6b1e1f32f8b02e
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-04-22 23:49:38 +0300

    Add files via upload

---
+++ ExerciseObjectsAndClasses/MoreExerciseP04.RawData/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MoreExerciseP04.RawData
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
