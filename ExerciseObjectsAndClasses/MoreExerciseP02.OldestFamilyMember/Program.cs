de860d5dd4e7c27f1a02346315b34fa6a53f94d0
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-04-22 23:49:13 +0300

    Add files via upload

---
+++ ExerciseObjectsAndClasses/MoreExerciseP02.OldestFamilyMember/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MoreExerciseP02.OldestFamilyMember
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
