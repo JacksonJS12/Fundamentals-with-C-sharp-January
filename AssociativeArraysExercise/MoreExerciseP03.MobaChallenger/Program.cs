6608b7e7e4763366d0782f0d3a3e23a2d88e3c99
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-04-23 11:16:39 +0300

    Add files via upload

---
+++ AssociativeArraysExercise/MoreExerciseP03.MobaChallenger/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MoreExerciseP03.MobaChallenger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
