e6689f8ef810097166bc779799a37dd81ab136d4
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-04-23 11:16:00 +0300

    Add files via upload

---
+++ AssociativeArraysExercise/MoreExerciseP01.Ranking/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MoreExerciseP01.Ranking
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
