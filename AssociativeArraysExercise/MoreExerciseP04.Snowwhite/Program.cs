ec1cadd8e81948bb56923589601c855f776c06e2
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-04-23 11:16:59 +0300

    Add files via upload

---
+++ AssociativeArraysExercise/MoreExerciseP04.Snowwhite/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MoreExerciseP04.Snowwhite
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
