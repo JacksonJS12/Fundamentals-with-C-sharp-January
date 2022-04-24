72a357ca6ba7d4b041a4f8f306505dee78369e1e
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-04-24 22:56:25 +0300

    Add files via upload

---
+++ ExerciseList/MoreExerciseP05.DrumSet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MoreExerciseP05.DrumSet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
