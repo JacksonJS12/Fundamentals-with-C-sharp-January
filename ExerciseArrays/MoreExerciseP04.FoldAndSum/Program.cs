9a240049b7dcd7ed3056c1e4b0d6e800089615aa
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-04-26 22:59:14 +0300

    Update Program.cs

--- ExerciseArrays/MoreExerciseP04.FoldAndSum/Program.cs
+++ ExerciseArrays/MoreExerciseP04.FoldAndSum/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using System;
+using System.Linq;
 
 namespace MoreExerciseP04.FoldAndSum
 {
@@ -6,7 +7,47 @@ namespace MoreExerciseP04.FoldAndSum
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] input = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] firstRow = new int[input.Length / 2];
+            int[] secondRow = new int[input.Length / 2];
+
+            int[] result = new int[firstRow.Length];
+
+            int[] firstPart = new int[input.Length / 4];
+            for (int i = 0; i < firstRow.Length / 2; i++)
+            {
+                firstPart.Append(input[i]);
+            }
+            firstPart.Reverse();
+            for (int i = 0; i < firstPart.Length; i++)
+            {
+                firstRow.Append(firstPart[i]);
+            }
+            int[] secondPart = new int[input.Length / 4];
+            for (int i = input.Length - 1; i >= input.Length / 4; i--)
+            {
+                secondPart.Append(input[i]);
+            }
+            secondPart.Reverse();
+
+            for (int i = 0; i < secondPart.Length; i++)
+            {
+                firstRow.Append(secondPart[i]);
+            }
+
+
+            for (int i = 0; i < firstRow.Length; i++)
+            {
+                result[i] = firstRow[i] + secondRow[i];
+            }
+
+            Console.WriteLine(string.Join(" ", result));
         }
+       
     }
+    
 }
