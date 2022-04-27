e6e3524c1b1d845c9cab4bef7d817ff4dc234107
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-04-27 23:14:43 +0300

    Update Program.cs

--- ExerciseArrays/MoreExerciseP04.FoldAndSum/Program.cs
+++ ExerciseArrays/MoreExerciseP04.FoldAndSum/Program.cs
@@ -12,34 +12,18 @@ namespace MoreExerciseP04.FoldAndSum
                 .Select(int.Parse)
                 .ToArray();
 
-            int[] firstRow = new int[input.Length / 2];
-            int[] secondRow = new int[input.Length / 2];
+
+
+            int[] firstPart = FirstPart(input);
+            int[] secondPart = SecondPart(input);
+
+            int[] firstRow = FirstRow(input, firstPart, secondPart);
+            int[] secondRow = SecondRow(input, firstPart, secondPart, firstRow);
+
+
+
 
             int[] result = new int[firstRow.Length];
-
-            int[] firstPart = new int[input.Length / 4];
-            for (int i = 0; i < firstRow.Length / 2; i++)
-            {
-                firstPart.Append(input[i]);
-            }
-            firstPart.Reverse();
-            for (int i = 0; i < firstPart.Length; i++)
-            {
-                firstRow.Append(firstPart[i]);
-            }
-            int[] secondPart = new int[input.Length / 4];
-            for (int i = input.Length - 1; i >= input.Length / 4; i--)
-            {
-                secondPart.Append(input[i]);
-            }
-            secondPart.Reverse();
-
-            for (int i = 0; i < secondPart.Length; i++)
-            {
-                firstRow.Append(secondPart[i]);
-            }
-
-
             for (int i = 0; i < firstRow.Length; i++)
             {
                 result[i] = firstRow[i] + secondRow[i];
@@ -47,7 +31,56 @@ namespace MoreExerciseP04.FoldAndSum
 
             Console.WriteLine(string.Join(" ", result));
         }
-       
+        static int[] FirstPart(int[] input)
+        {
+            int[] firstPart = new int[input.Length / 4];
+            for (int i = 0; i < input.Length / 4; i++)
+            {
+                firstPart[i] = input[i];
+            }
+            Array.Reverse(firstPart);
+
+            return firstPart;
+        }
+
+        static int[] SecondPart(int[] input)
+        {
+            int[] secondPart = Array.Empty<int>().ToArray();
+            for (int i = input.Length - 1; i > input.Length - 1 - (input.Length / 4); i--)
+            {
+                secondPart = secondPart.Append(input[i]).ToArray();
+            }
+
+            return secondPart;
+        }
+
+        static int[] FirstRow(int[] input, int[] firstPart, int[] secondPart)
+        {
+
+            int[] firstRow = firstPart.Concat(secondPart).ToArray();
+
+
+            return firstRow;
+        }
+
+        static int[] SecondRow(int[] input, int[] firstPart, int[] secondPart, int[] firstRow)
+        {
+            int[] secondRow = Array.Empty<int>();
+
+
+            for (int i = 0; i < firstRow.Length; i++)
+            {
+             bool flag = input.Contains(firstRow[i]);
+                if (flag == true)
+                {
+                    input = input.Where(val => val != firstRow[i]).ToArray();
+                }
+
+            }
+
+            secondRow = input;
+            return secondRow;
+        }
     }
-    
+
 }
