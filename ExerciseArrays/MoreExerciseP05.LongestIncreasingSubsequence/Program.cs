af479de4da170fedfb6e7a5489c819e9bb8ea153
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-05-01 23:39:44 +0300

    Add files via upload

--- ExerciseArrays/MoreExerciseP05.LongestIncreasingSubsequence/Program.cs
+++ ExerciseArrays/MoreExerciseP05.LongestIncreasingSubsequence/Program.cs
@@ -1,12 +1,45 @@
 ﻿using System;
+using System.Linq;
 
-namespace MoreExerciseP05.DrumSet
+namespace MoreExerciseP05.LongestIncreasingSubsequence
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] input = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] lis = new int[input.Length];
+            int lastNum = 0;
+            int prevNum = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (i == 0)
+                {
+                    lis[0] = input[0];
+                    lastNum = input[0];
+                    prevNum = input[0];
+                    continue;
+                }
+
+                if (lastNum < input[i])
+                {
+                    lis[i] = input[i];
+                    lastNum = input[i];
+                    prevNum = input[i - 1];
+                }
+                else if(prevNum < input[i] && lastNum > input[i])
+                {
+                    lis[i-1] = input[i];
+                    lastNum = input[i];
+                    prevNum = input[i - 1];
+                    
+                }
+
+            }
         }
     }
 }
