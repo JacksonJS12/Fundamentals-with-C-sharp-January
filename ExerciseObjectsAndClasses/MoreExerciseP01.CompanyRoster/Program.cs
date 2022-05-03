849cd2dceaa099ac2166c95a53027b26cb033631
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-05-03 23:29:59 +0300

    Update Program.cs

--- ExerciseObjectsAndClasses/MoreExerciseP01.CompanyRoster/Program.cs
+++ ExerciseObjectsAndClasses/MoreExerciseP01.CompanyRoster/Program.cs
@@ -1,12 +1,45 @@
-﻿using System;
+using System;
+using System.Linq;
 
 namespace MoreExerciseP01.CompanyRoster
 {
     internal class Program
     {
+        class Employee
+        {
+            public Employee(string name, decimal salary, string department)
+            {
+                this.Name = name;
+                this.Salary = salary;
+                this.Department = department;
+            }
+            public string Name { get; set; }
+            public decimal Salary { get; set; }
+            public string Department { get; set; }
+            
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int num = int.Parse(Console.ReadLine());
+
+            
+            for (int i = 0; i < num; i++)
+            {
+                string[] information = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                string name = information[0];
+                decimal salary = decimal.Parse(information[1]);
+                string department = information[2];
+
+
+            }
+       
+
+            Console.WriteLine($"Highest Average Salary: ");
+            Console.WriteLine();
+            Console.WriteLine();
         }
     }
 }
