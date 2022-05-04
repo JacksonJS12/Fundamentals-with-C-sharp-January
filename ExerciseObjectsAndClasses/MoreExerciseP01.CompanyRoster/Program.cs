32fc980800ef084cd22a9c9e8a99f6716290cf2c
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-05-04 23:33:37 +0300

    Update Program.cs

--- ExerciseObjectsAndClasses/MoreExerciseP01.CompanyRoster/Program.cs
+++ ExerciseObjectsAndClasses/MoreExerciseP01.CompanyRoster/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace MoreExerciseP01.CompanyRoster
@@ -7,39 +8,64 @@ namespace MoreExerciseP01.CompanyRoster
     {
         class Employee
         {
-            public Employee(string name, decimal salary, string department)
+            public string Name { get; set; }
+            public decimal Salary { get; set; }
+            public Employee(string name, decimal salary)
             {
                 this.Name = name;
                 this.Salary = salary;
-                this.Department = department;
             }
-            public string Name { get; set; }
-            public decimal Salary { get; set; }
-            public string Department { get; set; }
-            
         }
+        class Department
+        {
+            public string DepartmentName { get; set; }
+            public List<Employee> Employees { get; set; } = new List<Employee>();
+            public decimal TotalSalaries { get; set; }
+
+            public void AddNewEmployee(string name, decimal salary)
+            {
+                this.TotalSalaries += salary;
+                this.Employees.Add(new Employee(name, salary));
+            }
+
+            public Department(string departmentName)
+            {
+                this.DepartmentName = departmentName;
+            }
+        }
+
+
         static void Main(string[] args)
         {
+            List<Department> deparments = new List<Department>();
             int num = int.Parse(Console.ReadLine());
 
-            
+
             for (int i = 0; i < num; i++)
             {
                 string[] information = Console.ReadLine()
                     .Split(' ', StringSplitOptions.RemoveEmptyEntries)
                     .ToArray();
 
-                string name = information[0];
-                decimal salary = decimal.Parse(information[1]);
-                string department = information[2];
+                
+                if(!deparments.Any(d => d.DepartmentName == information[2]))
+                {
+                    deparments.Add(new Department(information[2]));
+                }
 
+                deparments.Find(d => d.DepartmentName == information[2]).AddNewEmployee(information[0], decimal.Parse(information[1]));
 
             }
-       
 
-            Console.WriteLine($"Highest Average Salary: ");
-            Console.WriteLine();
-            Console.WriteLine();
+            Department bestDepartment = deparments.OrderByDescending(d => d.TotalSalaries / d.Employees.Count()).First();
+
+    
+
+            Console.WriteLine($"Highest Average Salary: {bestDepartment.DepartmentName}");
+            foreach (var employee in bestDepartment.Employees.OrderByDescending(e => e.Salary))
+            {
+                Console.WriteLine($"{employee.Name} {employee.Salary:f2}");
+            }
         }
     }
 }
