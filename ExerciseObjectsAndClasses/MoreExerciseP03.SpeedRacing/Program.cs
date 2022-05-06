585920fe8effa5a1fee49c186bcbfecde64f943f
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-05-06 21:54:19 +0300

    Update Program.cs

--- ExerciseObjectsAndClasses/MoreExerciseP03.SpeedRacing/Program.cs
+++ ExerciseObjectsAndClasses/MoreExerciseP03.SpeedRacing/Program.cs
@@ -1,12 +1,38 @@
-﻿using System;
+using System;
+using System.Linq;
 
 namespace MoreExerciseP03.SpeedRacing
 {
     internal class Program
     {
+        class Car
+        {
+            public string Model { get; set; }
+            public int Fuel { get; set; }
+            public double FuelConsumptionPerKilometer { get; set; }
+            public int TraveledDistance { get; set; }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int num = int.Parse(Console.ReadLine());
+            for (int i = 0; i < num; i++)
+            {
+                string car = Console.ReadLine();
+
+                string command = Console.ReadLine();
+                while (command != "End")
+                {
+                    string[] carInfo = command
+                        .Split()
+                        .ToArray();
+
+                    string cmdType = carInfo[0];
+                    string carModel = carInfo[1];
+                    int amountOfKilometers = int.Parse(carInfo[2]);
+
+
+                }
+            }
         }
     }
 }
