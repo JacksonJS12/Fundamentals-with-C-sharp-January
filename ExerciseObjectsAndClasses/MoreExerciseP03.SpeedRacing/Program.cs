9bbf0838e6a33767e6d59a42c1ee19d6fdcec84a
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-05-07 23:24:57 +0300

    Update Program.cs

--- ExerciseObjectsAndClasses/MoreExerciseP03.SpeedRacing/Program.cs
+++ ExerciseObjectsAndClasses/MoreExerciseP03.SpeedRacing/Program.cs
@@ -8,27 +8,43 @@ namespace MoreExerciseP03.SpeedRacing
         class Car
         {
             public string Model { get; set; }
-            public int Fuel { get; set; }
+            public int FuelAmount { get; set; }
             public double FuelConsumptionPerKilometer { get; set; }
             public int TraveledDistance { get; set; }
+            public Car(string model, int fuelAmount, double fuelConsumptionPerKilometer, int traveledDistance)
+            {
+                this.Model = model;
+                this.FuelAmount = fuelAmount;
+                this.FuelConsumptionPerKilometer = fuelConsumptionPerKilometer;
+                this.TraveledDistance = traveledDistance;
+            }
         }
         static void Main(string[] args)
         {
             int num = int.Parse(Console.ReadLine());
             for (int i = 0; i < num; i++)
             {
-                string car = Console.ReadLine();
+                string[] carsInfo = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                string model = carsInfo[0];
+                int fuelAmout = int.Parse(carsInfo[1]);
+                double fuelConsumptionFor1km = double.Parse(carsInfo[2]);
+
 
                 string command = Console.ReadLine();
+                int traveledDistance = 0;
                 while (command != "End")
                 {
-                    string[] carInfo = command
+                    Car car = new Car(model, fuelAmout, fuelConsumptionFor1km, traveledDistance);
+
+                    string[] cmdInfo = command
                         .Split()
                         .ToArray();
 
-                    string cmdType = carInfo[0];
-                    string carModel = carInfo[1];
-                    int amountOfKilometers = int.Parse(carInfo[2]);
+                    string carModel = cmdInfo[1];
+                    int amountOfKm = int.Parse(cmdInfo[2]);
 
 
                 }
