f3a9f0a714225aef7ba14232d3a42d4813c19cc9
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-05-09 23:57:38 +0300

    Update Program.cs

--- ExerciseObjectsAndClasses/MoreExerciseP04.RawData/Program.cs
+++ ExerciseObjectsAndClasses/MoreExerciseP04.RawData/Program.cs
@@ -1,12 +1,43 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace MoreExerciseP04.RawData
 {
     internal class Program
     {
+        class Car
+        {
+            public string Model { get; set; }
+            class Engine
+            {
+                public int EngineSpeed { get; set; }
+                public int EnginePower { get; set; }
+            }
+            class Cargo
+            {
+                public int CargoWeight { get; set; }
+                public int CargoType { get; set; }
+            }
+
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int numOfCars = int.Parse(Console.ReadLine());
+            List<Car> cars = new List<Car>();
+
+            for (int i = 0; i < numOfCars; i++)
+            {
+                string[] carParams = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                string model = carParams[0];
+                int engineSpeed = int.Parse(carParams[1]);
+                int enginePower = int.Parse(carParams[2]);
+                int cargoWeight = int.Parse(carParams[3]);
+                string cargoType = carParams[4];
+            }
         }
     }
 }
