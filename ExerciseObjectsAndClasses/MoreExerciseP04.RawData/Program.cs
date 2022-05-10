3cb7dff3ac83a2785d30a7a0412bb5ab8d3d4b4e
Deniz Ivanov <99989417+JacksonJS12@users.noreply.github.com>
2022-05-10 23:14:18 +0300

    Update Program.cs

--- ExerciseObjectsAndClasses/MoreExerciseP04.RawData/Program.cs
+++ ExerciseObjectsAndClasses/MoreExerciseP04.RawData/Program.cs
@@ -9,18 +9,60 @@ namespace MoreExerciseP04.RawData
         class Car
         {
             public string Model { get; set; }
-            class Engine
+
+            public class Engine
             {
                 public int EngineSpeed { get; set; }
                 public int EnginePower { get; set; }
+                public Engine(int engineSpeed, int enginePower)
+                {
+                    this.EngineSpeed = engineSpeed;
+                    this.EnginePower = enginePower;
+                }
+
             }
-            class Cargo
+            public Engine EngineSpeedAndPower { get; set; }
+          
+            public class Cargo
             {
                 public int CargoWeight { get; set; }
-                public int CargoType { get; set; }
+                public string CargoType { get; set; }
+                public Cargo(int cargoWeight, string cargoType)
+                {
+                    this.CargoWeight = cargoWeight;
+                    this.CargoType = cargoType;
+                }
+            }
+            public Cargo CargoWeightAndType { get; set; }
+
+            public Car(string model, int engineSpeed, int enginePower, int cargoWeight, string cargoType)
+            {
+                this.Model = model;
+                this.EngineSpeedAndPower = new Engine(engineSpeed, enginePower);
+                this.CargoWeightAndType = new Cargo(cargoWeight, cargoType);
+            }
+           
+            public void Print(Car car)
+            {
+                if (this.CargoWeightAndType.CargoType == "fragile")
+                {
+                    if (this.CargoWeightAndType.CargoWeight < 1000)
+                    {
+                        Console.WriteLine(car.Model);
+                    }
+                   
+                }
+                else if (this.CargoWeightAndType.CargoType == "flamable")
+                {
+                    if (this.EngineSpeedAndPower.EnginePower > 250)
+                    {
+                        Console.WriteLine(car.Model);
+                    }
+                }
             }
 
         }
+
         static void Main(string[] args)
         {
             int numOfCars = int.Parse(Console.ReadLine());
@@ -37,6 +79,15 @@ namespace MoreExerciseP04.RawData
                 int enginePower = int.Parse(carParams[2]);
                 int cargoWeight = int.Parse(carParams[3]);
                 string cargoType = carParams[4];
+
+                Car car = new Car(model, engineSpeed, enginePower, cargoWeight, cargoType);
+                cars.Add(car);
+
+            }
+
+            foreach (var car in cars)
+            {
+                car.Print(car);
             }
         }
     }
